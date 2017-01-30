cf12dd55b69cdb3e61919cb562be1f3918e8de2e
Tyler Mills <tmills@monin.com>
2017-01-30 00:12:32 -0500

    Fix class refactoring miss

--- src/app/code/local/Etre/Botblock/Model/Observer.php
+++ src/app/code/local/Etre/Botblock/Model/Observer.php
@@ -2,16 +2,14 @@
 
 use Nabble\SemaltBlocker\Blocker;
 
-class Ere_Botblock_Model_Observer {
-    public function BlockerInit($observer){
-        try {
-           Blocker::protect();
-            $blocked = Blocker::blocked();
-            if($blocked){
-                echo Blocker::explain();
-            }
-        }catch (Exception $e){
-            dd($e->getMessage());
+class Etre_Botblock_Model_Observer
+{
+    public function BlockerInit($observer)
+    {
+        Blocker::protect();
+        $blocked = Blocker::blocked();
+        if ($blocked) {
+            echo Blocker::explain();
         }
     }
 }
